a69eb1aab5e2408bc82e1cb15d50da9b3066d330
Daniel Quinn <danquinndev@gmail.com>
2024-09-01 21:53:25 -0400

    first commit

---
+++ keystone.ts
@@ -0,0 +1,49 @@
+// Welcome to Keystone!
+//
+// This file is what Keystone uses as the entry-point to your headless backend
+//
+// Keystone imports the default export of this file, expecting a Keystone configuration object
+//   you can find out more at https://keystonejs.com/docs/apis/config
+
+import { config } from '@keystone-6/core'
+
+// to keep this file tidy, we define our schema in a different file
+import { lists } from './schema'
+
+// authentication is configured separately here too, but you might move this elsewhere
+// when you write your list-level access control functions, as they typically rely on session data
+import { withAuth, session } from './auth'
+
+export default withAuth(
+  config({
+    db: {
+      // we're using sqlite for the fastest startup experience
+      //   for more information on what database might be appropriate for you
+      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
+      provider: 'sqlite',
+      url: 'file:./keystone.db',
+    },
+    lists,
+    session,
+    storage: {
+      release_image: {
+        kind: 'local',
+        type: 'image',
+        generateUrl: path => `http://localhost:3000/release_images${path}`,
+        serverRoute: {
+          path: '/release_images',
+        },
+        storagePath: 'public/release_images',
+      },
+      track_mp3: {
+        kind: 'local',
+        type: 'file',
+        generateUrl: path => `http://localhost:3000/track_mp3s${path}`,
+        serverRoute: {
+          path: '/track_mp3s',
+        },
+        storagePath: 'public/track_mp3s',
+      },
+    }
+  })
+)
